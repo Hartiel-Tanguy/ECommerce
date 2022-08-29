97871ff0b1e8786c239ac21fa1bf853927ca16ad
Antoine <tanguy.hartiel.1@gmail.com>
2022-08-29 12:30:01 +0200

    log

--- shop/urls.py
+++ shop/urls.py
@@ -14,18 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from store.views import index
 from store.views import product_detail
 from shop import settings
+from accounts.views import signup
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('product/<str:slug>/', product_detail, name='product'),
-
+    path('signup/', signup, name='signup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
